ff7248c1c461dee5977fbf90811c9199177445e7
Destroit <66379677+Destroit@users.noreply.github.com>
2021-12-14 21:46:08 +0300

    RIP PubSubClient

--- src/ESP8266_ButtonVer.cpp
+++ src/ESP8266_ButtonVer.cpp
@@ -1,70 +1,92 @@
-#include <ESP8266WiFi.h>        //Библиотека Wi-Fi для модуля ESP8266
-#include <WiFiUdp.h>            //Библиотека реализации протокола UDP через Wi-Fi
-#include <NTPClient.h>          //Библиотека синхронизации часов
-#include <Wire.h>               //Библеотека реализации протокола I2C для OLED SSD1306
-#include <OneWire.h>            //Библиотека реализации однопроводная линии передачи данных OneWire
-#include <DallasTemperature.h>  // Для работы с датчиками Dallas     
+/*
+1.Wi-Fi for ESP8266
+2.UDP protocol realisation
+3.Time synchroniastion using NTP
+4.MQTT
+5.I2C for OLED SSD1306
+6.OneWire for D18B20
+7.for D18B20 
+8.for buttons
+9.for SSD1306 OLED 
+*/
+
+#include <ESP8266WiFi.h>       
+#include <PubSubClient.h>       
+#include <Wire.h>               
+#include <OneWire.h>            
+#include <DallasTemperature.h>  
 #include <OneButton.h>         
 #include "SSD1306Wire.h"
+//#include <WiFiUdp.h>            
+//#include <NTPClient.h>   
 
-const char* SSID = "ssid";
-const char* PASSWORD = "pwd"; 
+const char* SSID = "SysCat_AP";
+const char* PASSWORD = "shirokod"; 
+const char* MQTT_SERVER = "192.168.100.116";
+const char* MQTT_LOGIN = "mosquitto";
+const char* MQTT_PASSWORD = "mosquitto";
+const char* MQTT_SENDTPC =  "home/CurrentTemp";
+const char* MQTT_RECIEVETPC = "home/SetTemp";
+const char* MQTT_BACKSENDTPC = "home/BackSetTemp";
+const int MQTT_PORT = 1883;
 const int RELAY_PIN = 12;
 const int BTN_PIN = 14;
 const int ONEWIRE_BUS = 13;
+
+/*
 const long UTC_OFFSET_SECONDS = 10800;
 const float NIGHT_L_TEMP_BORDER = 16;
 const float NIGHT_H_TEMP_BORDER = 20;
 const int PM_NIGHT_TIME = 19;
 const int AM_NIGHT_TIME = 5;
+*/
 
 String modeString;
-String DispWifiStatus;
+String dispWifiStatus;
+WiFiClient espClient;
+PubSubClient client(espClient);
 unsigned long lastTemp = 0;
-float Sensor_temp = 0;
-float SumTemp = 0;
-int hours = 0;
-int minutes = 0;
-int seconds = 0;
+unsigned long lastReconnectAttempt = 0;
+float sensorTemp = 0;
+float sumTemp = 0;
 int modecount = 1;
 int reqTemp = 18;
 int deviceCount = 0;
+char msgT[7] = "";
+char backreqT[7] = "";
 
-WiFiUDP ntpUdp;
-NTPClient timeClient(ntpUdp, "pool.ntp.org", UTC_OFFSET_SECONDS);
+/*
+int hours = 0;
+int minutes = 0;
+int seconds = 0;
+*/
+
+
 OneWire DallasOneWire(ONEWIRE_BUS);
 DallasTemperature sensors(&DallasOneWire);
-OneButton button(BTN_PIN, true);
+OneButton button(BTN_PIN, true, true);
 SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_128_32);
 
-void setup_wifi()
+void offlineActionList();
+/*
+WiFiUDP ntpUdp;
+NTPClient timeClient(ntpUdp, "pool.ntp.org", UTC_OFFSET_SECONDS);
+*/
+
+void IRAM_ATTR interruptbtn()
 {
+  button.tick();
+}
+void wifiSetup()
+{
+  Serial.println("entering wifiSetup");
+  WiFi.disconnect();
+  WiFi.mode(WIFI_STA);
   WiFi.begin(SSID, PASSWORD);
   while (WiFi.status() != WL_CONNECTED)
   {
-    delay(500);
-  }
-}
-
-void wifi_current()
-{
-  switch(WiFi.status())
-  {
-    case WL_CONNECTED:
-    DispWifiStatus = "WiFi +";
-    break;
-
-    case WL_CONNECTION_LOST:
-    DispWifiStatus = "no WiFi";
-    break;
-
-    case WL_NO_SSID_AVAIL:
-    DispWifiStatus = "WiFi -";
-    break;
-
-    case WL_CONNECT_FAILED:
-    DispWifiStatus = "Wifi x";
-    break;
+    Serial.println(".");
+    offlineActionList();
   }
 }
 
@@ -87,10 +109,39 @@ void switchclick()
   }
 }
 
+void recievedTemp(char* topic, byte* payload, unsigned int length)
+{
+  Serial.println("entering recievedTemp");
+  char arrayToInt[length] = ""; 
+  Serial.print("Message arrived from ");
+  Serial.println(topic);
+  for (unsigned int i = 0; i < length; i++) 
+  {
+    Serial.print((char)payload[i]);
+    arrayToInt[i] = (char)payload [i];
+  }
+  reqTemp = atoi(arrayToInt);
+}
+
+bool reconnect()
+{
+  Serial.println("reconnecting");
+  offlineActionList();
+  if (client.connect("ESP8266Client")) 
+  {
+    Serial.println("connected");
+    client.publish(MQTT_SENDTPC, msgT); //Публикация температуры на сервер
+    client.subscribe(MQTT_RECIEVETPC);     //Подписка на топик
+  } 
+  return client.connected();
+}
+
 void setup()
 {
-  //Serial.begin(115200);
+  Serial.begin(115200);
   pinMode(RELAY_PIN, OUTPUT);  
+
+  randomSeed(micros());
 
   sensors.begin();
   sensors.requestTemperatures();
@@ -104,12 +155,30 @@ void setup()
   display.setTextAlignment(TEXT_ALIGN_LEFT);
   display.setFont(ArialMT_Plain_10);
 
-  setup_wifi();
+  wifiSetup();
+  
+  client.setServer(MQTT_SERVER, MQTT_PORT);
+  client.setCallback(recievedTemp);
+  attachInterrupt(digitalPinToInterrupt(BTN_PIN), interruptbtn, CHANGE);
 }
 
-void disp()
+void displayPrepare()
 {
-  switch (modecount)
+  //Prepares data for being displayed 
+  String reqstr = String(reqTemp);
+  String realstr = String(sumTemp);
+  //String StrTime = String(hours)+":"+String(minutes)+":"+String(seconds);
+  display.drawString(0, 0, "Set T:"+reqstr);
+  display.drawString(0, 10, "Real T:"+realstr);
+  display.drawString(48, 0, modeString);
+  //display.drawString(0, 20, StrTime);
+  display.drawString(60, 0, dispWifiStatus);
+
+}
+
+void modeCurrent()
+{
+   switch (modecount)
     {
     case 1:
     modeString = "+";
@@ -123,21 +192,46 @@ void disp()
     modecount = 1;
     break;
     }
-  String reqstr = String(reqTemp);
-  String realstr = String(SumTemp);
-  String StrTime = String(hours)+":"+String(minutes)+":"+String(seconds);
-  display.drawString(0, 0, "Set T:"+reqstr);
-  display.drawString(0, 10, "Real T:"+realstr);
-  display.drawString(48, 0, modeString);
-  display.drawString(0, 20, StrTime);
-  display.drawString(60, 0, DispWifiStatus);
+}
+
+
+void wifiCurrent()
+{
+  //Prepares current Wifi status for display
+  switch(WiFi.status())
+  {
+    case WL_CONNECTED:
+    dispWifiStatus = "WL +";
+    break;
+
+    case WL_CONNECTION_LOST:
+    dispWifiStatus = "WL lst";
+    break;
+
+    case WL_NO_SSID_AVAIL:
+    dispWifiStatus = "WL id";
+    break;
+
+    case WL_CONNECT_FAILED:
+    dispWifiStatus = "WL fld";
+    break;
+
+    case WL_IDLE_STATUS:
+    dispWifiStatus = "WL idl";
+    break;
+
+    case WL_DISCONNECTED:
+    dispWifiStatus = "WL dis";
+    break;
+  }
 }
 
 void getTemp()
 {
+  Serial.println("entering getTemp");
   sensors.requestTemperatures();
-  Sensor_temp = 0;
-  SumTemp = 0;
+  sensorTemp = 0;
+  sumTemp = 0;
   deviceCount = sensors.getDeviceCount();
   Serial.print("Devices found: ");
   Serial.println(deviceCount);
@@ -147,72 +241,106 @@ void getTemp()
   {
     DeviceAddress Address18b20;
     sensors.getAddress(Address18b20, i);
-    Sensor_temp = sensors.getTempC(Address18b20);
-    /*Serial.print("Sensor ");                   //Значение датчиков по отдельности
+    sensorTemp = sensors.getTempC(Address18b20);
+    /*
+    Sensors' values to port
+    Serial.print("Sensor ");                   
     Serial.print(i + 1);
     Serial.print(": ");
-    Serial.println(Sensor_temp);*/
-    SumTemp += Sensor_temp;
+    Serial.println(Sensor_temp);
+    */
+    sumTemp += sensorTemp;
   }
-  SumTemp /= deviceCount;
-  Serial.println(SumTemp);
+  sumTemp /= deviceCount;
+  //Serial.println(sumTemp);
+  dtostrf(sumTemp, 6, 2, msgT);
 } 
 
-void relay()
+void relayHandler()
 {
-  if (SumTemp < reqTemp){
+  if (sumTemp < (reqTemp-1)){
     digitalWrite(RELAY_PIN, HIGH);
   }
-  if (SumTemp > reqTemp){
+  if (sumTemp > (reqTemp+1)){
     digitalWrite(RELAY_PIN, LOW);
   }
-  /*if ((hours>PM_NIGHT_TIME) or (hours<AM_NIGHT_TIME))
+  /*
+  Night mode (Needs RTC, in progress)
+  if ((hours>PM_NIGHT_TIME) or (hours<AM_NIGHT_TIME))
   {
-    if (SumTemp < NIGHT_L_TEMP_BORDER)
+    if (sumTemp < NIGHT_L_TEMP_BORDER)
     {
       digitalWrite(RELAY_PIN, HIGH);
     }
-    if (SumTemp > NIGHT_H_TEMP_BORDER)
+    if (sumTemp > NIGHT_H_TEMP_BORDER)
     {
       digitalWrite(RELAY_PIN, LOW);
     }
   }
   else{
-    if(SumTemp < reqTemp-2)
+    if(sumTemp < reqTemp-2)
     {
     digitalWrite(RELAY_PIN, HIGH);
     }
-    if(SumTemp > reqTemp+2)
+    if(sumTemp > reqTemp+2)
     {
     digitalWrite(RELAY_PIN, LOW);
     }
   }*/
 }
 
-void time()
+/*
+void getNtpTime()
 {
   timeClient.update();
   hours = timeClient.getHours();
   minutes = timeClient.getMinutes();
   seconds = timeClient.getSeconds();
 }
+*/
 
-
-void loop()
+void offlineActionList()
 {
+  //Tasks that could be done without WiFi
   button.tick();
-  wifi_current();
-  disp();
-  if (WiFi.status() == WL_CONNECTED)
-  {
-    time();
-  }
+  wifiCurrent();
+  modeCurrent();
+  displayPrepare();
   if(millis()-lastTemp > 5000)
   {
     getTemp();
     lastTemp = millis();
   }
-  relay();
+  relayHandler();
   display.display();
   display.clear();
 }
+
+
+void loop()
+{
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    wifiSetup();
+  }
+  else 
+  {
+    if (!client.connected()) {
+    long now = millis();
+    if (now - lastReconnectAttempt > 5000) {
+      lastReconnectAttempt = now;
+      if (reconnect()) {
+        lastReconnectAttempt = 0;
+      }
+     }
+    }
+    else
+    {
+    offlineActionList();
+    client.loop();
+    dtostrf(reqTemp, 6, 2, backreqT);
+    client.publish(MQTT_SENDTPC, msgT);
+    client.publish(MQTT_BACKSENDTPC, backreqT);
+    }
+  }
+} 
