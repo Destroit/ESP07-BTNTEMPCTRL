489471581586fa3687ad95a475f4409dcc263e7f
Destroit <66379677+Destroit@users.noreply.github.com>
2022-03-25 23:59:05 +0300

    Working Async

--- src/ESP8266_ButtonVer.cpp
+++ src/ESP8266_ButtonVer.cpp
@@ -1,350 +1,298 @@
-/*
-1.Wi-Fi for ESP8266
-2.UDP protocol realisation
-3.Time synchroniastion using NTP
-4.MQTT
-5.I2C for OLED SSD1306
-6.OneWire for D18B20
-7.for D18B20 
-8.for buttons
-9.for SSD1306 OLED 
-*/
 
+#include <ESP8266WiFi.h>
 #include <Ticker.h>
-#include <ESP8266WiFi.h>       
-#include <PubSubClient.h>       
-#include <Wire.h>               
-#include <OneWire.h>            
-#include <DallasTemperature.h>  
-#include <OneButton.h>         
+#include <Wire.h>
+#include <OneWire.h>
+#include "EncButton.h"
+#include "DallasTemperature.h"
+#include "AsyncMqttClient.h"
 #include "SSD1306Wire.h"
-//#include <WiFiUdp.h>            
-//#include <NTPClient.h>   
 
-const char* SSID = "Smart_Home";
-const char* PASSWORD = "shirokod"; 
-const char* MQTT_SERVER = "192.168.120.92";
-const char* MQTT_LOGIN = "mosquitto";
-const char* MQTT_PASSWORD = "mosquitto";
-const char* MQTT_SENDTPC =  "home/CurrentTemp";
-const char* MQTT_RECIEVETPC = "home/SetTemp";
-const char* MQTT_BACKSENDTPC = "home/BackSetTemp";
-const int MQTT_PORT = 1883;
-const int RELAY_PIN = 12;
-const int BTN_PIN = 14;
-const int ONEWIRE_BUS = 13;
+#define WIFI_SSID "ssid"
+#define WIFI_PASSWORD "pwd"
+#define MQTT_HOST IPAddress(192, 168, 100, 116)
+#define MQTT_PORT 1883
+#define MQTT_LOGIN "mosquitto"
+#define MQTT_PASSWORD "mosquitto"
+#define MQTT_SENDTPC "home/CurrentTemp"
+#define MQTT_RECIEVETPC "home/SetTemp"
+#define MQTT_BACKSENDTPC "home/BackSetTemp"
+#define RELAY_PIN 2
+#define ONEWIRE_BUS 13
+#define ENC_MODE 1
+#define ENC_A 12
+#define ENC_B 14
 
-/*
-const long UTC_OFFSET_SECONDS = 10800;
-const float NIGHT_L_TEMP_BORDER = 16;
-const float NIGHT_H_TEMP_BORDER = 20;
-const int PM_NIGHT_TIME = 19;
-const int AM_NIGHT_TIME = 5;
-*/
-
-String modeString;
-String dispWifiStatus;
-WiFiClient espClient;
-PubSubClient client(espClient);
-unsigned long lastTemp = 0;
-unsigned long lastReconnectAttempt = 0;
+const uint8_t relayInterval = 2; // temperature gap in Celsius to reduce relay switching 
+char msgT[7] = "";               // to send 
+char backreqT[7] = "";
+int8_t reqTemp = 18;
+uint8_t deviceCount = 0;
+uint64_t lastTempCall = 0; 
+uint64_t lastBackReq = 0;
 float sensorTemp = 0;
 float sumTemp = 0;
-int modecount = 1;
-int reqTemp = 18;
-int deviceCount = 0;
-char msgT[7] = "";
-char backreqT[7] = "";
 
-/*
-int hours = 0;
-int minutes = 0;
-int seconds = 0;
-*/
-
-
+EncButton<ENC_MODE, ENC_A, ENC_B> enc;
+String dispWifiStatus;
+AsyncMqttClient espClient;
+Ticker mqttReconnectTimer;
+Ticker wifiReconnectTimer;
+WiFiEventHandler wifiConnectHandler;
+WiFiEventHandler wifiDisconnectHandler;
 OneWire DallasOneWire(ONEWIRE_BUS);
 DallasTemperature sensors(&DallasOneWire);
-OneButton button(BTN_PIN, true, true);
-SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_128_32);
-void offlineActionList();
-void displayPrepare();
-Ticker btnTimer;
-/*
-WiFiUDP ntpUdp;
-NTPClient timeClient(ntpUdp, "pool.ntp.org", UTC_OFFSET_SECONDS);
-*/
+SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_128_64);
 
-void btntick()
+void connectToWifi();
+void connectToMqtt();
+void onWifiConnect(const WiFiEventStationModeGotIP& event);
+void onWifiDisconnect(const WiFiEventStationModeDisconnected& event);
+void onMqttConnect(bool sessionPresent);
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason);
+void onMqttSubscribe(uint16_t packetId, uint8_t qos);
+void onMqttUnsubscribe(uint16_t packetId);
+void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total);
+void onMqttPublish(uint16_t packetId);
+void displayDraw();
+void wifiCurrent();
+void getTemp();
+
+// Encoder via interrupt
+void IRAM_ATTR isr()
 {
-  button.tick();
+  enc.tickISR();
 }
 
-void wifiSetup()
+void rightInc()
 {
-  Serial.println("entering wifiSetup");
-  WiFi.disconnect();
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(SSID, PASSWORD);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.println(".");
-    offlineActionList();
-  }
+  ++reqTemp;
 }
 
-void modeclick()
+void leftDec()
 {
-  modecount++;
-}
-
-void switchclick()
-{
-  switch (modecount)
-  {
-   case 1:
-   reqTemp++;
-   break;
-
-   case 2:
-   reqTemp--;
-   break;
-  }
-}
-
-void recievedTemp(char* topic, byte* payload, unsigned int length)
-{
-  Serial.println("entering recievedTemp");
-  char arrayToInt[length] = ""; 
-  Serial.print("Message arrived from ");
-  Serial.println(topic);
-  for (unsigned int i = 0; i < length; i++) 
-  {
-    Serial.print((char)payload[i]);
-    arrayToInt[i] = (char)payload [i];
-  }
-  reqTemp = atoi(arrayToInt);
-}
-
-bool reconnect()
-{
-  Serial.println("reconnecting");
-  offlineActionList();
-  if (client.connect("ESP8266Client")) 
-  {
-    Serial.println("connected");
-    client.publish(MQTT_SENDTPC, msgT); //Публикация температуры на сервер
-    client.subscribe(MQTT_RECIEVETPC);     //Подписка на топик
-  } 
-  return client.connected();
+  --reqTemp;
 }
 
 void setup()
 {
-  Serial.begin(115200);
-  pinMode(RELAY_PIN, OUTPUT);  
-
-  btnTimer.attach(0.6, btntick);
-
-  randomSeed(micros());
-
-  sensors.begin();
-  sensors.requestTemperatures();
-
-  button.setDebounceTicks(50);
-  button.attachLongPressStop(modeclick);
-  button.attachClick(switchclick);
-
-  display.init();
-  display.flipScreenVertically();
-  display.setTextAlignment(TEXT_ALIGN_LEFT);
-  display.setFont(ArialMT_Plain_10);
-
-  wifiSetup();
-  
-  client.setServer(MQTT_SERVER, MQTT_PORT);
-  client.setCallback(recievedTemp);
+    Serial.begin(115200);
+    Serial.println("Serial OK");
+    pinMode(RELAY_PIN, OUTPUT);
+    display.init();
+    enc.attach(LEFT_HANDLER, leftDec);
+    enc.attach(RIGHT_HANDLER, rightInc);
+    attachInterrupt(0, isr, CHANGE);
+    attachInterrupt(1, isr, CHANGE);
+    wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
+    wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
+    espClient.onConnect(onMqttConnect);
+    espClient.onDisconnect(onMqttDisconnect);
+    espClient.onSubscribe(onMqttSubscribe);
+    espClient.onUnsubscribe(onMqttUnsubscribe);
+    espClient.onMessage(onMqttMessage);
+    espClient.onPublish(onMqttPublish);
+    espClient.setServer(MQTT_HOST, MQTT_PORT);
+    connectToWifi();
 }
 
-void displayPrepare()
+void connectToWifi()
 {
-  //Prepares data for being displayed 
-  String reqstr = String(reqTemp);
-  String realstr = String(sumTemp);
-  //String StrTime = String(hours)+":"+String(minutes)+":"+String(seconds);
-  display.drawString(0, 0, "Set T:"+reqstr);
-  display.drawString(0, 10, "Real T:"+realstr);
-  display.drawString(48, 0, modeString);
-  //display.drawString(0, 20, StrTime);
-  display.drawString(60, 0, dispWifiStatus);
-
+    Serial.println("Connecting to WiFi...");
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 }
 
-void modeCurrent()
+void connectToMqtt()
 {
-   switch (modecount)
+    Serial.println("Connecting to MQTT...");
+    espClient.connect();
+}
+
+void onWifiConnect(const WiFiEventStationModeGotIP& event)
+{
+    Serial.println("Connected to Wi-Fi.");
+    connectToMqtt();
+}
+
+void onWifiDisconnect(const WiFiEventStationModeDisconnected& event)
+{
+    Serial.println("Disconnected from Wi-Fi.");
+    mqttReconnectTimer.detach();
+    wifiReconnectTimer.once(2, connectToWifi);
+}
+
+void onMqttConnect(bool sessionPresent)
+{
+    Serial.println("Connected to MQTT.");
+    Serial.print("Session present: ");
+    Serial.println(sessionPresent);
+    uint16_t packetIdSub = espClient.subscribe(MQTT_RECIEVETPC, 1);
+    Serial.print("Subscribing MQTT, packetId: ");
+    Serial.println(packetIdSub);
+    uint16_t packetIdPub1 = espClient.publish(MQTT_SENDTPC, 1, true, msgT);
+    Serial.print("Publishing Temperature, packetId: ");
+    Serial.println(packetIdPub1);
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason)
+{ 
+    Serial.println("Disconnected from MQTT.");
+    if (WiFi.isConnected()) 
     {
-    case 1:
-    modeString = "+";
-    break;
-
-    case 2:
-    modeString = "-";
-    break;
-
-    default:
-    modecount = 1;
-    break;
+        mqttReconnectTimer.once(2, connectToMqtt);
     }
 }
 
+void onMqttSubscribe(uint16_t packetId, uint8_t qos)
+{
+    Serial.println("Subscribe acknowledged.");
+    Serial.print("  packetId: ");
+    Serial.println(packetId);
+    Serial.print("  qos: ");
+    Serial.println(qos);
+}
+
+void onMqttUnsubscribe(uint16_t packetId)
+{
+    Serial.println("Unsubscribe acknowledged.");
+    Serial.print("  packetId: ");
+    Serial.println(packetId);
+}
+
+void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total)
+{
+    Serial.println("Mesasage received.");
+    Serial.print("  topic: ");
+    Serial.println(topic);
+    Serial.print("  qos: ");
+    Serial.println(properties.qos);
+    Serial.print("  dup: ");
+    Serial.println(properties.dup);
+    Serial.print("  retain: ");
+    Serial.println(properties.retain);
+    Serial.print("  len: ");
+    Serial.println(len);
+    Serial.print("  index: ");
+    Serial.println(index);
+    Serial.print("  total: ");
+    Serial.println(total);
+    char arrayToInt[len] = ""; 
+    for (unsigned int i = 0; i < len; i++) 
+    {
+        Serial.print(payload[i]);
+        arrayToInt[i] = payload [i];
+    }
+    reqTemp = atoi(arrayToInt);
+}
+
+void onMqttPublish(uint16_t packetId)
+{
+    Serial.println("Publish acknowledged.");
+    Serial.print("  packetId: ");
+    Serial.println(packetId);
+}
 
 void wifiCurrent()
 {
-  //Prepares current Wifi status for display
-  switch(WiFi.status())
-  {
-    case WL_CONNECTED:
-    dispWifiStatus = "WL +";
-    break;
-
-    case WL_CONNECTION_LOST:
-    dispWifiStatus = "WL lst";
-    break;
-
-    case WL_NO_SSID_AVAIL:
-    dispWifiStatus = "WL id";
-    break;
-
-    case WL_CONNECT_FAILED:
-    dispWifiStatus = "WL fld";
-    break;
-
-    case WL_IDLE_STATUS:
-    dispWifiStatus = "WL idl";
-    break;
-
-    case WL_DISCONNECTED:
-    dispWifiStatus = "WL dis";
-    break;
-  }
+    switch(WiFi.status())
+    {
+        case WL_CONNECTED:
+        dispWifiStatus = "WiFi OK";
+        break;
+        
+        case WL_CONNECTION_LOST:
+        dispWifiStatus = "WiFi lost";
+        break;
+        
+        case WL_NO_SSID_AVAIL:
+        dispWifiStatus = "WiFi ssid";
+        break;
+        
+        case WL_CONNECT_FAILED:
+        dispWifiStatus = "WiFi failed";
+        break;
+        
+        case WL_IDLE_STATUS:
+        dispWifiStatus = "WiFi idle";
+        break;
+        
+        case WL_DISCONNECTED:
+        dispWifiStatus = "WiFi disconnected";
+        break;
+    }
 }
 
-void getTemp()
+//Prepares display
+void displayDraw()
 {
-  Serial.println("entering getTemp");
-  sensors.requestTemperatures();
-  sensorTemp = 0;
-  sumTemp = 0;
-  deviceCount = sensors.getDeviceCount();
-  Serial.print("Devices found: ");
-  Serial.println(deviceCount);
-  DeviceAddress Address18b20;
-  sensors.getAddress(Address18b20, 1);
-  for (int i = 0; i<= deviceCount-1; i++)
-  {
-    DeviceAddress Address18b20;
-    sensors.getAddress(Address18b20, i);
-    sensorTemp = sensors.getTempC(Address18b20);
-    /*
-    Sensors' values to port
-    Serial.print("Sensor ");                   
-    Serial.print(i + 1);
-    Serial.print(": ");
-    Serial.println(Sensor_temp);
-    */
-    sumTemp += sensorTemp;
-  }
-  sumTemp /= deviceCount;
-  //Serial.println(sumTemp);
-  dtostrf(sumTemp, 6, 2, msgT);
-} 
-
-void relayHandler()
-{
-  if (sumTemp < (reqTemp-1)){
-    digitalWrite(RELAY_PIN, HIGH);
-  }
-  if (sumTemp > (reqTemp+1)){
-    digitalWrite(RELAY_PIN, LOW);
-  }
-  /*
-  Night mode (Needs RTC, in progress)
-  if ((hours>PM_NIGHT_TIME) or (hours<AM_NIGHT_TIME))
-  {
-    if (sumTemp < NIGHT_L_TEMP_BORDER)
-    {
-      digitalWrite(RELAY_PIN, HIGH);
-    }
-    if (sumTemp > NIGHT_H_TEMP_BORDER)
-    {
-      digitalWrite(RELAY_PIN, LOW);
-    }
-  }
-  else{
-    if(sumTemp < reqTemp-2)
-    {
-    digitalWrite(RELAY_PIN, HIGH);
-    }
-    if(sumTemp > reqTemp+2)
-    {
-    digitalWrite(RELAY_PIN, LOW);
-    }
-  }*/
-}
-
-/*
-void getNtpTime()
-{
-  timeClient.update();
-  hours = timeClient.getHours();
-  minutes = timeClient.getMinutes();
-  seconds = timeClient.getSeconds();
-}
-*/
-
-void offlineActionList()
-{
-  //Tasks that could be done without WiFi
-  button.tick();
-  wifiCurrent();
-  modeCurrent();
-  displayPrepare();
-  if(millis()-lastTemp > 5000)
-  {
-    getTemp();
-    lastTemp = millis();
-  }
-  relayHandler();
+  String reqstr = String(reqTemp);
+  String realstr = String(sumTemp);
+  display.drawString(0, 0, "Set T:"+reqstr);
+  display.drawString(0, 10, "Real T:"+realstr);
+  display.drawString(0, 20, dispWifiStatus);
   display.display();
   display.clear();
+
 }
 
+//Gets temperature from sensors
+void getTemp()
+{
+    Serial.println("entering getTemp");
+    sensors.requestTemperatures();
+    sensorTemp = 0;
+    sumTemp = 0;
+    deviceCount = sensors.getDeviceCount();
+    Serial.print("Devices found: ");
+    Serial.println(deviceCount);
+    DeviceAddress Address18b20;
+    sensors.getAddress(Address18b20, 1);
+    for (int i = 0; i<= deviceCount-1; i++)
+    {
+        DeviceAddress Address18b20;
+        sensors.getAddress(Address18b20, i);
+        sensorTemp = sensors.getTempC(Address18b20);
+        Serial.print("Sensor ");                   
+        Serial.print(i + 1);
+        Serial.print(": ");
+        Serial.println(sensorTemp);
+        sumTemp += sensorTemp;
+    }
+    sumTemp /= deviceCount;
+    Serial.println(sumTemp);
+    dtostrf(sumTemp, 6, 2, msgT);
+}
+
+//Relay reaction on temperature changes
+void relay()
+{
+  if (reqTemp > (sumTemp + relayInterval))
+  {
+    digitalWrite(RELAY_PIN, HIGH);
+  }
+  else if (reqTemp < (sumTemp - relayInterval))
+  {
+    digitalWrite(RELAY_PIN, LOW);
+  }
+}
 
 void loop()
 {
-  if (WiFi.status() != WL_CONNECTED)
-  {
-    wifiSetup();
-  }
-  else 
-  {
-    if (!client.connected()) {
-    long now = millis();
-    if (now - lastReconnectAttempt > 5000) {
-      lastReconnectAttempt = now;
-      if (reconnect()) {
-        lastReconnectAttempt = 0;
-      }
-     }
-    }
-    else
+    if (millis() - lastTempCall > 5000)
     {
-    offlineActionList();
-    client.loop();
-    dtostrf(reqTemp, 6, 2, backreqT);
-    client.publish(MQTT_SENDTPC, msgT);
-    client.publish(MQTT_BACKSENDTPC, backreqT);
+        lastTempCall = millis();
+        getTemp();
+        espClient.publish(MQTT_SENDTPC, 1, true, msgT);
     }
-  }
-} 
+
+    if (millis() - lastBackReq > 2000)
+    {
+        lastBackReq = millis();
+        dtostrf(reqTemp, 6, 2, backreqT);
+        espClient.publish(MQTT_BACKSENDTPC, 1 , true, backreqT);
+    }
+    relay();
+    enc.tick();
+    wifiCurrent();
+    displayDraw();
+}
